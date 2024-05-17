805746e9e64cb6a9a5f821c7d35bf7f4c61da73b
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:00:45 +0400

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -4,12 +4,42 @@ Animal::Animal(int age, double weight) : age(age), weight(weight) {}
 
 Animal::~Animal() {}
 
-int Animal::getAge() const 
+// Move constructor
+Animal::Animal(Animal&& other)
+    : age(other.age), weight(other.weight)
 {
-    return age;
+    other.age = 0;
+    other.weight = 0.0;
 }
 
-double Animal::getWeight() const 
+// Move assignment operator
+Animal& Animal::operator=(Animal&& other)
 {
-    return weight;
+    if (this != &other)
+    {
+        age = other.age;
+        weight = other.weight;
+
+        other.age = 0;
+        other.weight = 0.0;
+    }
+    return *this;
 }
+
+// Copy constructor
+Animal::Animal(const Animal& other)
+    : age(other.age), weight(other.weight) {}
+
+// Copy assignment operator
+Animal& Animal::operator=(const Animal& other)
+{
+    if (this != &other)
+    {
+        age = other.age;
+        weight = other.weight;
+    }
+    return *this;
+}
+
+int Animal::getAge() const { return age; }
+double Animal::getWeight() const { return weight; }
