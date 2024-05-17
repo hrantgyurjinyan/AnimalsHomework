2e15e360d90aad0f6da7fe4fa3260128c1941208
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:02:51 +0400

    Update amphibian.cpp

--- amphibian.cpp
+++ amphibian.cpp
@@ -1,3 +1,48 @@
 #include "amphibian.hpp"
 
-Amphibian::Amphibian(int age, double weight, bool isAquatic, bool isTerrestrial) : Animal(age, weight), isAquatic(isAquatic), isTerrestrial(isTerrestrial) {}
+Amphibian::Amphibian(int age, double weight, bool isAquatic, bool isTerrestrial)
+    : Animal(age, weight), isAquatic(isAquatic), isTerrestrial(isTerrestrial) {}
+
+// Move constructor
+Amphibian::Amphibian(Amphibian&& other)
+    : Animal(other.age, other.weight), isAquatic(other.isAquatic), isTerrestrial(other.isTerrestrial)
+{
+    other.age = 0;
+    other.weight = 0.0;
+    other.isAquatic = false;
+    other.isTerrestrial = false;
+}
+
+// Move assignment operator
+Amphibian& Amphibian::operator=(Amphibian&& other)
+{
+    if (this != &other)
+    {
+        age = other.age;
+        weight = other.weight;
+        isAquatic = other.isAquatic;
+        isTerrestrial = other.isTerrestrial;
+
+        other.age = 0;
+        other.weight = 0.0;
+        other.isAquatic = false;
+        other.isTerrestrial = false;
+    }
+    return *this;
+}
+
+// Copy constructor
+Amphibian::Amphibian(const Amphibian& other)
+    : Animal(other), isAquatic(other.isAquatic), isTerrestrial(other.isTerrestrial) {}
+
+// Copy assignment operator
+Amphibian& Amphibian::operator=(const Amphibian& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        isAquatic = other.isAquatic;
+        isTerrestrial = other.isTerrestrial;
+    }
+    return *this;
+}
