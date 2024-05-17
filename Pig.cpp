25d449aff1ebd4b4fc4c1d7e8bfbba1b8a7ae4f5
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:02:13 +0400

    Update Pig.cpp

--- Pig.cpp
+++ Pig.cpp
@@ -1,12 +1,37 @@
 #include "Pig.hpp"
-
 #include <iostream>
 
 Pig::Pig(int age, double weight) : Animal(age, weight) {}
 
 void Pig::voice() const 
 {
-    std::cout << "Oink!" << std::endl;
+    std::cout << "Oink" << std::endl;
 }
 
 Pig::~Pig() {}
+
+// Move constructor
+Pig::Pig(Pig&& other) noexcept : Animal(std::move(other)) {}
+
+// Move assignment operator
+Pig& Pig::operator=(Pig&& other) noexcept
+{
+    if (this != &other)
+    {
+        Animal::operator=(std::move(other));
+    }
+    return *this;
+}
+
+// Copy constructor
+Pig::Pig(const Pig& other) : Animal(other) {}
+
+// Copy assignment operator
+Pig& Pig::operator=(const Pig& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+    }
+    return *this;
+}
