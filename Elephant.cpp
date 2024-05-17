3904a4439219ab5f757358fc45f8db6f0bc8a6fe
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:01:49 +0400

    Update Elephant.cpp

--- Elephant.cpp
+++ Elephant.cpp
@@ -1,12 +1,37 @@
 #include "Elephant.hpp"
-
 #include <iostream>
 
 Elephant::Elephant(int age, double weight) : Animal(age, weight) {}
 
 void Elephant::voice() const 
 {
-    std::cout << "Trumpet!" << std::endl;
+    std::cout << "Trumpet" << std::endl;
 }
 
 Elephant::~Elephant() {}
+
+// Move constructor
+Elephant::Elephant(Elephant&& other) noexcept : Animal(std::move(other)) {}
+
+// Move assignment operator
+Elephant& Elephant::operator=(Elephant&& other) noexcept
+{
+    if (this != &other)
+    {
+        Animal::operator=(std::move(other));
+    }
+    return *this;
+}
+
+// Copy constructor
+Elephant::Elephant(const Elephant& other) : Animal(other) {}
+
+// Copy assignment operator
+Elephant& Elephant::operator=(const Elephant& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+    }
+    return *this;
+}
