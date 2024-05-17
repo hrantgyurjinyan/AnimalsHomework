3d2bb2112bbde063cdcbaaa7cbe48276afec8324
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:01:16 +0400

    Update Cat.cpp

--- Cat.cpp
+++ Cat.cpp
@@ -1,12 +1,46 @@
 #include "Cat.hpp"
-
 #include <iostream>
 
 Cat::Cat(int age, double weight) : Animal(age, weight) {}
 
 void Cat::voice() const 
 {
-    std::cout << "Meow!" << std::endl;
+    std::cout << "Meow" << std::endl;
 }
 
 Cat::~Cat() {}
+
+// Move constructor
+Cat::Cat(Cat&& other)
+    : Animal(other.age, other.weight)
+{
+    other.age = 0;
+    other.weight = 0.0;
+}
+
+// Move assignment operator
+Cat& Cat::operator=(Cat&& other)
+{
+    if (this != &other)
+    {
+        age = other.age;
+        weight = other.weight;
+
+        other.age = 0;
+        other.weight = 0.0;
+    }
+    return *this;
+}
+
+// Copy constructor
+Cat::Cat(const Cat& other) : Animal(other) {}
+
+// Copy assignment operator
+Cat& Cat::operator=(const Cat& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+    }
+    return *this;
+}
