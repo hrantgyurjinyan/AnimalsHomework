a99335372cd08e6250b4bcda920b23744479628b
hrantgyurjinyan <148892988+hrantgyurjinyan@users.noreply.github.com>
2024-05-17 14:01:33 +0400

    Update Dog.cpp

--- Dog.cpp
+++ Dog.cpp
@@ -5,7 +5,33 @@ Dog::Dog(int age, double weight) : Animal(age, weight) {}
 
 void Dog::voice() const 
 {
-    std::cout << "Woof!" << std::endl;
+    std::cout << "Woof" << std::endl;
 }
 
 Dog::~Dog() {}
+
+// Move constructor
+Dog::Dog(Dog&& other) noexcept : Animal(std::move(other)) {}
+
+// Move assignment operator
+Dog& Dog::operator=(Dog&& other) noexcept
+{
+    if (this != &other)
+    {
+        Animal::operator=(std::move(other));
+    }
+    return *this;
+}
+
+// Copy constructor
+Dog::Dog(const Dog& other) : Animal(other) {}
+
+// Copy assignment operator
+Dog& Dog::operator=(const Dog& other)
+{
+    if (this != &other)
+    {
+        Animal::operator=(other);
+    }
+    return *this;
+}
